78c4c454b6f7f975d1f6cc8c9ed152ef3fc2a3f9
Damien Courcoux <damiencourcoux27@free.fr>
2019-02-19 16:54:03 +0100

    a

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -12,7 +12,7 @@
 
 
       <nav id="nav" class="navbar navbar-expand-lg navbar-dark ">
-        <a id="titrelogo" class="navbar-brand" href="index.php">Movies_2</a>
+        <a id="titrelogo" class="navbar-brand" href="index.php">Movies_2 Coté Admin</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
