859e5c835eb6c20a5736c764bc8e6feca40f0c63
Damien Courcoux <damiencourcoux27@free.fr>
2019-02-19 11:41:19 +0100

    a jour

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,30 @@
   <body>
     <header>
 
-      <h1>Movies_2</h1>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Movies_2</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">• S'identifier</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">• Créer un compte</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Recherche" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+          </form>
+        </div>
+      </nav>
 
     </header>
     <div class="wrap">
--- index.php
+++ index.php
@@ -4,9 +4,4 @@ include('inc/pdo.php');
 
 include('inc/header.php');?>
 
-
-<a href="login.php">• Login</a>
-
-<a href="register.php">• Créer un compte</a>
-
 <?php include('inc/footer.php');
