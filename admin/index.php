a151a38dd11ab37e4d90367afb258ea3036d37e7
Ano27 <p.duval27350@hotmail.fr>
2019-02-19 16:53:05 +0100

    .

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,11 @@ include('../inc/fonction.php');
 // --------------------------------------------------------------------------------
 //PDO => connexion base de donne
 include('../inc/pdo.php');
+if (isAdmin()) {
+  //
+}else {
+  die('403');
+}
 //---------------------------------------------------------------------------------
 //traitement php
  include('inc/header.php'); ?>
