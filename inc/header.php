d3d72516846e7db022760bb83da4f16260eb7366
Damien Courcoux <damiencourcoux27@free.fr>
2019-02-19 12:50:12 +0100

    a jour

--- inc/header.php
+++ inc/header.php
@@ -11,7 +11,7 @@
     <header>
 
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">Movies_2</a>
+        <a id="titrelogo" class="navbar-brand" href="index.php">Movies_2</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,7 +33,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="profil.php">• Profil</a>
                 </li>
-                <?php echo 'Bonjour'.' '. $_SESSION['user']['pseudo']; ?>
+                <li class="nav-item">
+                  <?php echo 'Bonjour'.' '. $_SESSION['user']['pseudo']; ?>
+                </li>
               <?php } ?>
               <?php if (isAdmin()) { ?>
                 <li class="nav-item">
