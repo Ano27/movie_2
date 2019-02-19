3e0b7713cc8d76c42f75616a5ba7b3765bb5d9fe
Alexandre DECLERCQ <alexandre.de-clercq@laposte.net>
2019-02-19 11:44:44 +0100

    Ajout forgetpassword + modifiedpassword

---
+++ forgetpassword.php
@@ -0,0 +1,43 @@
+<?php
+include('inc/pdo.php');
+include('inc/request.php');
+require('inc/fonction.php');
+if (!empty($_POST['submitted'])) {
+  // Protection faille XSS
+  $email = trim(strip_tags($_POST['email']));
+  if (empty($email) || (filter_var($email,FILTER_VALIDATE_EMAIL) === FALSE)) {
+    $error['email'] = 'Adresse email invalide';
+  } else {
+    //verifier si user existe
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':email',$email,PDO::PARAM_STR);
+    $query->execute();
+    $user = $query->fetch();
+    if (!empty($user)) {
+      $tokenEncode = urlencode($user['token']);
+      $emailEncode = urlencode($user['email']);
+      $url = 'http://localhost/movie/movie_2/modifiedpassword.php?email='.$emailEncode . '&token='.$tokenEncode;
+      $html = '<p>Veuillez cliquer sur le lien ci-dessous</p>';
+      $html .= '<p><a href="'.$url.'">Click ici pour modifier ton mot de passe</a></p>';
+      echo $html;
+      die('Attention');
+    } else {
+      $error['email'] = 'Ne marche pas';
+    }
+  }
+}
+
+
+
+
+
+include_once('inc/header.php'); ?>
+<form class="" method="post">
+  <label for="email">Email *</label>
+  <span class="error"><?php if(!empty($error['email'])) { echo $error['email']; } ?></span>
+  <input type="text" name="email" value="<?php if(!empty($_POST['email'])) { echo $_POST['email']; } ?>">
+  <br>
+  <input class="" type="submit" name="submitted" value="Connexion">
+</form>
+<?php include_once('inc/footer.php');
