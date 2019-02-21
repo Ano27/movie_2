5d2bb54c96b5e24b7b1d3f49c895df75a02ed265
Alexandre DECLERCQ <alexandre.de-clercq@laposte.net>
2019-02-21 09:41:13 +0100

    fkdldf

--- index.php
+++ index.php
@@ -8,21 +8,12 @@ require('inc/fonction.php');
 
   <?php foreach ($movies as $movie): ?>
 
-
-    <!-- <div class="affichefilm"><h2><?php echo $movie['title']; ?></h2>
+    <div class="affichefilm"><h2><?php echo $movie['title']; ?></h2>
     <h2 id="titrefilm"><a href="detail.php?id=<?php echo $movie['id']; ?>"></h2>
-    <?php affiche($movie)?></div> -->
-
-    <?php
-    // PROF 
-      // ici c'est le slug et non id du film a faire passer
-      // penser dans le fichier detail d'aller chercher le film à partir du slug et non de id
-     ?>
-
-      <?php echo '<li class="affichefilm">'.'<a id="titrefilm" href="detail.php?id='.$movie['id'].'" >' .$movie['title'] . '</a></li>';?>
-      <?php echo  affiche($movie)?>
+    <?php affiche($movie)?></div>
 
   <?php endforeach; ?>
+  
   <a id="plusdefilm" href="index.php">« + de films ! »</a>
 
 <?php include('inc/footer.php');
