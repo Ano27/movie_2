71c90d6f7afe12e52838c99485320c17993caf14
Ano27 <p.duval27350@hotmail.fr>
2019-02-21 09:32:56 +0100

    fixation affich

--- index.php
+++ index.php
@@ -10,12 +10,9 @@ require('inc/fonction.php');
   <?php foreach ($movies as $movie): ?>
 
 
-    <!-- <div class="affichefilm"><h2><?php echo $movie['title']; ?></h2>
-    <h2 id="titrefilm"><a href="detail.php?id=<?php echo $movie['id']; ?>"></h2>
-    <?php affiche($movie)?></div> -->
-
-      <?php echo '<li class="affichefilm">'.'<a id="titrefilm" href="detail.php?id='.$movie['id'].'" >' .$movie['title'] . '</a></li>';?>
-      <?php echo  affiche($movie)?>
+    <div class="affichefilm"><h2><?php echo $movie['title']; ?></h2>
+    <h2 id="titrefilm"><a class="blanco" href="detail.php?id=<?php echo $movie['id']; ?>"></h2>
+    <?php affiche($movie)?></div>
 
   <?php endforeach; ?>
   <a id="plusdefilm" href="index.php">« + de films ! »</a>
